728eb64b2d8d39f0958e3ce0fdfde9324a53f8ff
Hasnine Mamud <hasninehemel85@gmail.com>
2024-02-03 12:52:11 +0600

    First commit

---
+++ project.py
@@ -0,0 +1,19 @@
+import os
+files = os. listdir("submission")
+submitted_roll = []
+for file in files:
+    div = file.split("_")
+    roll = div[1]
+    submitted_roll.append(roll)
+while True:
+
+    roll_to_check = input("Enter the roll to check: &  to quite enter q = ")
+
+    if roll_to_check == "q":
+        exit()
+
+    if roll_to_check in submitted_roll:
+        print(f"Roll: {roll_to_check} submit there assignment")
+    else:
+        print(f"Roll: {roll_to_check} not submit there assignment")
+
