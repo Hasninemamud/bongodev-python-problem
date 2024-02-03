3a2f1f0a17ee717d9c73d5636342f8f27e3c2156
Hasnine Mamud <hasninehemel85@gmail.com>
2024-02-03 18:30:07 +0600

    Try-Exception add

--- project.py
+++ project.py
@@ -5,15 +5,18 @@ for file in files:
     div = file.split("_")
     roll = div[1]
     submitted_roll.append(roll)
-while True:
+try:
+        while True:
 
-    roll_to_check = input("Enter the roll to check: &  to quite enter q = ")
+            roll_to_check = input("Enter the roll to check: &  to quite enter q = ")
 
-    if roll_to_check == "q":
-        exit()
+            if roll_to_check == "q":
+                exit()
 
-    if roll_to_check in submitted_roll:
-        print(f"Roll: {roll_to_check} submit there assignment")
-    else:
-        print(f"Roll: {roll_to_check} not submit there assignment")
+            if roll_to_check in submitted_roll:
+                print(f"Roll: {roll_to_check} submit there assignment")
+            else:
+                print(f"Roll: {roll_to_check} not submit there assignment")
+except Exception as e:
+    print("Othrwise ok")
 
