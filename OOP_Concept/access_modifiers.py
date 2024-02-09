b9d9eaf66b4ee00ff226da0787c0b635a8bf015a
Hasnine Mamud <hasninehemel85@gmail.com>
2024-02-09 22:14:04 +0600

    initial commit

--- OOP_Concept/access_modifiers.py
+++ OOP_Concept/access_modifiers.py
@@ -1,8 +1,16 @@
 class Student:
-    def __init__(self,name):
+    def __init__(self,name, rollnum,age):
         self.name = name
-    def display(self):
+        self._rollnum = rollnum #protected instance
+        self.__age = age
+        
+    def __display(self):
         print(f"Hi myself {self.name} from Student class")
-s1 = Student("Rahul")
-s1.display()
+class Branch(Student):
+    pass
+
+s1 = Branch("Rahul", 22, 15)
+print(s1.name)
+print(s1._rollnum)
+print(s1.__age)
         
---
+++ Pytest/test/test_add.py
@@ -0,0 +1,4 @@
+from sample.sample_test import add
+
+def test_add():
+    assert add(1 + 2) == 3
