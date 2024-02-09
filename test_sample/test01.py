a6ed068f9fd26abc1b6ca1249c5dc2d26027f8b7
Hasnine Mamud <hasninehemel85@gmail.com>
2024-02-09 12:13:56 +0600

    Unit testing using PyTest

---
+++ test_sample/test01.py
@@ -0,0 +1,5 @@
+from sampletest.add import add
+
+
+def test_num():
+    assert add(1, 2) == 3
---
+++ test_sample/test_sample.py
@@ -0,0 +1,8 @@
+from sampletest.add import add
+
+
+def test_num():
+    assert add(1, 2) == 3
+
+def test_str():
+    assert add("a", "b") == "abc"
